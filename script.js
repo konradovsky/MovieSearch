e5bac8a8d6a7105873d9560f2b01bb7a561c2045
MartynaStrzempek <martyna.strzempek@gmail.com>
2017-11-22 16:18:03 +0100

    display several movies

---
+++ script.js
@@ -0,0 +1,21 @@
+// function successCB(data) {
+// 	console.log("Success callback: " + data);
+// };
+//
+// function errorCB(data) {
+//         	console.log("Error callback: " + data);
+//     };
+//
+//     $('#searchButton').click(function () {
+//                 var searchTerm = $('#movieTitleInput').val(),
+//
+//                 options = { "query": searchTerm };
+//
+//             theMovieDb.search.getMovie(options, successCB, errorCB);
+//         });
+
+// var xhr = new XMLHttpRequest();
+// xhr.open("GET", "https://www.themoviedb.org/&api_key=a7f7d0f12e764bab89e2aeb212e2e9c4", false);
+// xhr.send();
+// console.log(xhr.status);
+// callback for successful movie search
